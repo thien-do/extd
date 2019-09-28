56fc1369e83a3d98f2215ee1c04dcefa5767dfd9
dvkndn <dvkndn@gmail.com>
2019-09-28 17:28:51 +0700

    build(root): add commitlint

---
+++ .commitlintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  rules: {
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [2, 'always'],
+    'header-max-length': [2, 'always', 72],
+    'scope-empty': [2, 'never'],
+    'scope-enum': [2, 'always', ['all', 'core', 'docs']],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-empty': [2, 'never'],
+    'type-enum': [2, 'always', ['build', 'chore', 'docs', 'feat', 'fix', 'test']]
+  }
+};
