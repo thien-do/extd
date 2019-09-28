bf81c059a76a61a1f7490ae052610a5e39fa9910
dvkndn <dvkndn@gmail.com>
2019-09-28 18:17:38 +0700

    build(root): add ts-eslint

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking"
+  ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react",
+    "@typescript-eslint"
+  ],
+  "rules": {
+    "@typescript-eslint/indent": ["error", 2],
+    "@typescript-eslint/no-floating-promises": ["error"],
+    "@typescript-eslint/no-magic-numbers": ["error"],
+    "@typescript-eslint/no-require-imports": ["error"],
+    "@typescript-eslint/quotes": ["error"],
+    "@typescript-eslint/semi": ["error"],
+    "indent": "off",
+    "linebreak-style": ["error", "unix"],
+    "no-magic-numbers": "off",
+    "quotes": "off",
+    "semi": "off"
+  }
+};
